13d87402c938e9f9e3efecce1e305a6cc8235709
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-25 00:14:01 -0600

    Add the audio manager service.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using strange.extensions.mediation.impl;
+
+public class AudioManager : View, IAudioManager
+{
+
+    [Inject]
+    public IEventManager EventManager { get; set; }
+
+    public AudioSource source;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        EventManager.AddListener<PlayAudioEvent>(HandlePlayAudio);
+    }
+
+    protected override void OnDestroy()
+    {
+        EventManager.RemoveListener<PlayAudioEvent>(HandlePlayAudio);
+    }
+
+    private void HandlePlayAudio(PlayAudioEvent e)
+    {
+        if (e.Clip != null)
+        {
+            source.clip = e.Clip;
+            source.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/IAudioManager.cs
@@ -0,0 +1,4 @@
+﻿
+public interface IAudioManager {
+
+}

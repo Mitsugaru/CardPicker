4112968cbdee85eaca506488c25d099eb29e47ef
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-25 00:13:24 -0600

    Added the Play Audio event.

---
+++ Assets/Scripts/Events/PlayAudioEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayAudioEvent : GameEvent
+{
+
+    private AudioClip audio;
+    public AudioClip Clip
+    {
+        get
+        {
+            return audio;
+        }
+    }
+
+    public PlayAudioEvent(AudioClip audio)
+    {
+        this.audio = audio;
+    }
+}
